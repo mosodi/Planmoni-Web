c11979317e4145eae5c66eaaa0141336e2a997fb
Martins Osodi <itsmartinsevans@gmail.com>
2025-06-08 19:16:16 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -731,7 +731,7 @@ function App() {
               See How It Works
             </div>
             <h3 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-gray-900">
-              In 3 steps you can
+              With just three steps you can
               <br />
               <span className="text-[#1F3A8A]">start using Planmoni </span>
             </h3>
